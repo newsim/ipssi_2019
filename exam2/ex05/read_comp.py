5ab240fab7702922bf138e17572184cd9291a76b
sim001 <simon.amoyal4@gmail.com>
2019-11-08 12:38:24 +0100

    ex5

--- exam2/ex05/read_comp.py
+++ exam2/ex05/read_comp.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
-    fichier = open("../docker-compose.yml", "a")
-    f
+fichier = open("../docker-compose.yml", "r")
+    
+for ligne in fichier:
+    ligne.strip('e')
+
+    print(ligne)
+    fichier.close
