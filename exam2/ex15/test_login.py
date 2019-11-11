fa82690b911e20e9b41de629f20b33f7b05f9532
sim001 <simon.amoyal4@gmail.com>
2019-11-11 21:16:33 +0100

    final

--- exam2/ex15/test_login.py
+++ exam2/ex15/test_login.py
@@ -8,8 +8,16 @@ import time
 
 import hashlib
 
+a = sys.argv
+
 if (hashlib.md5(sys.argv[1].encode('utf-8')).hexdigest()) == '82771f9740d5e024ab823c12a9b51289':
     print ('bon mot de passe')
 else:
     print ('mauvais mot de passe')
-    
+
+
+""" def login(a):
+    if (hashlib.md5(sys.argv[0].encode('utf-8')).hexdigest()) == '82771f9740d5e024ab823c12a9b51289':
+        return print ('bon mot de passe')
+    else:
+        return print ('mauvais mot de passe') """
