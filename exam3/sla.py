14b703b107033b3515e8fb172d01a28e59b811b1
sim001 <simon.amoyal4@gmail.com>
2019-12-06 12:45:51 +0100

    sla sans hh mm ss

---
+++ exam3/sla.py
@@ -0,0 +1,16 @@
+
+import os
+import datetime
+import sys
+from datetime import timedelta
+
+def sla(argument):
+   calcul =( (365.25*24*3600*1 - 365.25*24*3600*argument/100) )
+   print()
+   affichage = datetime.timedelta( 0, calcul)
+   return affichage
+
+
+
+if __name__ == '__main__':
+    print( sla( float(sys.argv[1])) )
