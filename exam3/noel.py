c0a596a5c7d00d8ccd4c3d49c36b19b45ad1bc1c
sim001 <simon.amoyal4@gmail.com>
2019-12-06 15:56:44 +0100

    saut de liogne

--- exam3/noel.py
+++ exam3/noel.py
@@ -10,10 +10,19 @@ def noel(date):
     date = sys.argv[1]
     date = datetime.strptime(date, "%d-%m-%Y")
     d =  date - datetime.today()
-    text = str(d.days) + ' days before christmas' 
-    return text
+    text = str(d.days) + ' days before christmas'  
+
+    # create a plain text calendar
+
+    year  = date.year
+    month = date.month
+    # calendar of month
+    cd = calendar.month(year, month)
+    res = text + "\n" + cd
+    return res
+
 
 
 
 if __name__ == '__main__':
-   print(noel(str(sys.argv[1])) )
+    print(noel( str(sys.argv[1])) )
